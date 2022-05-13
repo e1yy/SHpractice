0e5c5ae54fc9a63910d06b81b4644793e5de2421
Sanghun Park <guswls9769@naver.com>
2022-05-13 13:02:20 +0900

    Make Cat

--- src/Practice.java
+++ src/Practice.java
@@ -2,8 +2,10 @@
 public class Practice {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
 	}
 
 }
